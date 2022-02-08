12f08f5834d6b101328c936442c99fafbe6aa031
Cho SangHyun <98shcho@naver.com>
2022-02-08 20:19:16 +0900

    간단한 css 및 영화 삭제 기능 추가(filter메소드를 이용한)

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import ReviewList from "./ReviewList";
-import items from '../mock.json';
+import mockItems from '../mock.json';
 import { useState } from "react";
 
 function App(){
+    const [items, setItems] = useState(mockItems);
     // 정렬기준에 대한 state
     const [order, setOrder] = useState('createdAt');
     // 정렬기준을 통해 아이템 정렬
@@ -12,14 +13,17 @@ function App(){
     const handleNewestClick = () => setOrder('createdAt');
     const handleBestClick = () => setOrder('rating'); 
 
-
+    const handleDelete = (id) => {
+        const nextItems = items.filter((item) => item.id !== id);
+        setItems(nextItems);
+    }
     return(
         <div>
             <div>
                 <button onClick={handleNewestClick}>최신 순으로 보기</button>
                 <button onClick={handleBestClick}>평점 순으로 보기</button>
             </div>
-            <ReviewList items={sortedItems}/>
+            <ReviewList items={sortedItems} onDelete={handleDelete}/>
         </div>
     );
 }
--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -1,27 +1,32 @@
+import './ReviewList.css';
+
 function formatDate(value){
     const date = new Date(value);
     return date.getFullYear() + '.' + (date.getMonth() + 1) + '.' + date.getDate();
 }
 
-function ReviewListItem({item}){
+function ReviewListItem({item, onDelete}){
+    const handleDeleteClick = () => onDelete(item.id);
+
     return(
         <div className="ReviewListItem">
-            <img className="ReViewListItem-img" src={item.imgUrl} alt={item.title} />
+            <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title} />
             <div>
                 <h1>{item.title}</h1>
                 <p>{item.rating}</p>
                 <p>{formatDate(item.createdAt)}</p>
                 <p>{item.content}</p>
+                <button onClick={handleDeleteClick}>삭제</button>
             </div>
         </div>
     );
 }
 
-function ReviewList({items}){
+function ReviewList({items, onDelete}){
     return(
         // map 메소드 안에서 JSX요소를 리턴하면 마치 JSX를 여러 개 추가한 것처럼 동작
         <ul>{items.map((item) => {
-            return <li><ReviewListItem item={item} /></li>
+            return <li><ReviewListItem item={item} onDelete={onDelete}/></li>
         })}</ul>
     );
 }
