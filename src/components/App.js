3e5b1ca7aa18ee1a5094c848860f4c13dcc702c4
Cho SangHyun <98shcho@naver.com>
2022-02-08 16:54:43 +0900

    영화들 최신순으로 보기/평점 순으로 보기 기능 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import ReviewList from "./ReviewList";
 import items from '../mock.json';
+import { useState } from "react";
 
 function App(){
+    // 정렬기준에 대한 state
+    const [order, setOrder] = useState('createdAt');
+    // 정렬기준을 통해 아이템 정렬
+    const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+    // setter함수를 이용해 정렬기준을 바꾸는 메소드들
+    const handleNewestClick = () => setOrder('createdAt');
+    const handleBestClick = () => setOrder('rating'); 
+
+
     return(
         <div>
-            <ReviewList items={items}/>
+            <div>
+                <button onClick={handleNewestClick}>최신 순으로 보기</button>
+                <button onClick={handleBestClick}>평점 순으로 보기</button>
+            </div>
+            <ReviewList items={sortedItems}/>
         </div>
     );
 }
